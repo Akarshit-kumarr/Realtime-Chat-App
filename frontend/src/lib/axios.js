cf111eab4b1c4e7fe6442d6db8957e47f90fda8d
Akarshit-kumarr <aakarshitkumar2@gmail.com>
2026-06-05 13:56:41 +0530

    fix baseURL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://realtime-chat-app-io0h.onrender.com/api",
   withCredentials: true,
 });
